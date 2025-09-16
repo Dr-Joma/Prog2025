ecc98787203d8e10ec97a87136014cc4a9508f16
d_joma <jphantom927@gmail.com>
2025-09-16 11:06:26 +0200

    kezdet

---
+++ elso.py
@@ -0,0 +1,23 @@
+import random
+
+
+szamok = []
+
+# 100 elemu lista 1-99 kozott
+
+for i in range (100):
+    # szam generalas
+    rszam = random.randint(1, 100)
+
+    # szam elhelyezese a listaba
+    szamok.append(rszam)
+# ellenorzes
+print(szamok)
+
+# egyszamjatek
+jatek_szam = 0
+nem_talaltDB = 0
+
+# kitalalando szam
+kitalalando_szam = 
+
