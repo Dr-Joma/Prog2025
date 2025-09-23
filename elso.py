4f134f6a96cf2e202a131474e0682f5e2cae0860
d_joma <jphantom927@gmail.com>
2025-09-23 12:28:35 +0200

    moasikdik

--- elso.py
+++ elso.py
@@ -19,5 +19,23 @@ jatek_szam = 0
 nem_talaltDB = 0
 
 # kitalalando szam
-kitalalando_szam = input()
+kitalalando_szam = szamok[random.randint(0, len(szamok))]
 
+tipp = int(input("Kerek egy egesz szamot [1-100]: "))
+
+if (tipp == kitalalando_szam):
+    print("Fasza vagy")
+elif (tipp < kitalalando_szam):
+    print("nagyob")
+else:
+    print("kiseb")
+    
+while (tipp != kitalalando_szam):
+    tipp = int(input("Kerek egy egesz szamot [1-100]: "))
+    
+    if (tipp == kitalalando_szam):
+        print("Fasza vagy")
+    elif (tipp < kitalalando_szam):
+        print("nagyob")
+    else:
+        print("kiseb")
